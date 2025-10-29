df27856dcf427119af67027c5a2251717ee06ae8
herotchi <hidenochion+github@gmail.com>
2025-10-29 17:33:42 +0900

    sanctumの設定

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //axios.defaults.baseURL = "https://1944yamao-ka.sakura.ne.jp/shout/backend"; // Laravel側のURL
 axios.defaults.baseURL = "http://localhost:8000";
-axios.defaults.withCredentials = true; // Cookieを送信して認証を維持
+axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 //axios.defaults.xsrfCookieName = "ENC_XSRF-TOKEN";
 //axios.defaults.xsrfCookieName = "XSRF-TOKEN";
